5645e234c06587710dbc7eae590cec6ca5aa31d2
hmlyman <56166746+hmlyman@users.noreply.github.com>
2020-04-11 18:29:50 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,16 +4,36 @@ const first = document.querySelector("#first");
 const last = document.querySelector("#last");
 const diff = document.querySelector("#diff");
 const intersect = document.querySelector("#intersect");
+const zip = document.querySelector("#zip");
+const unzip = document.querySelector("#unzip");
+const union = document.querySelector("#union");
+const initial = document.querySelector("#initial");
+const rest = document.querySelector("#rest");
 
 const firstItems = _.first([1, 2, 3, 4], 2);
 const lastItems = _.last([1, 2, 3, 4], 2);
 const difference = _.difference(["bob", "dude", "loop"], ["dude", "loop"]);
 const intersection = _.intersection(["bob", "dude", "loop"], ["dude", "loop"]);
+const zipItems = _.zip([1, 2, 3, 4], ["bob", "dude", "loop", "geraldine"]);
+const unzipItems = _.unzip([
+  [1, "bob"],
+  [2, "dude"],
+  [3, "loop"],
+  [4, "geraldine"],
+]);
+const unionItems = _.union([1, 2, 3, 4], [3, 4, 5, 6]);
+const initialItems = _.initial([1, 2, 3, "tree", "flower"]);
+const restItems = _.rest([1, 2, 3, 4, "tree", "flower"], 3);
 
 first.innerHTML = JSON.stringify(firstItems);
 last.innerHTML = JSON.stringify(lastItems);
 diff.innerHTML = JSON.stringify(difference);
 intersect.innerHTML = JSON.stringify(intersection);
+zip.innerHTML = JSON.stringify(zipItems);
+unzip.innerHTML = JSON.stringify(unzipItems);
+union.innerHTML = JSON.stringify(unionItems);
+initial.innerHTML = JSON.stringify(initialItems);
+rest.innerHTML = JSON.stringify(restItems);
 
 //import { myMath } from "./myMath";
 //import { dice } from "./dice";
--- src/underDash.js
+++ src/underDash.js
@@ -15,7 +15,7 @@ export default (() => {
         throw new Error(
           `${JSON.stringify(
             n
-          )} is not a valid number of items. Pleasu use a number > 0`
+          )} is not a valid number of items. Please use a number > 0`
         );
       }
       return array.slice(0, n);
@@ -25,7 +25,7 @@ export default (() => {
         throw new Error(
           `${JSON.stringify(
             array
-          )} is not an array. The first function only works on arrays.`
+          )} is not an array. The last function only works on arrays.`
         );
       }
       if (typeof n === "undefined") {
@@ -35,7 +35,7 @@ export default (() => {
         throw new Error(
           `${JSON.stringify(
             n
-          )} is not a valid number of items. Pleasu use a number > 0`
+          )} is not a valid number of items. Please use a number > 0`
         );
       }
       return array.slice(Math.max(array.length - n, 0));
@@ -45,42 +45,119 @@ export default (() => {
         throw new Error(
           `${JSON.stringify(
             array1
-          )} is not an array. The first function only works on arrays.`
+          )} is not an array. The difference function only works on arrays.`
         );
       }
-      if (typeof n === "undefined") {
-        let a = [];
-        let diff = [];
-        for (let i = 0; i < array1.length; i++) {
-          a[array1[i]] = true;
-        }
-        for (let i = 0; i < array2.length; i++) {
-          if (a[array2[i]]) {
-            delete a[array2[i]];
-          } else {
-            a[array2[i]] = true;
-          }
-        }
-        for (let k in a) {
-          diff.push(k);
-        }
-        return diff;
+      let a = [];
+      let diff = [];
+      for (let i = 0; i < array1.length; i++) {
+        a[array1[i]] = true;
       }
+      for (let i = 0; i < array2.length; i++) {
+        if (a[array2[i]]) {
+          delete a[array2[i]];
+        } else {
+          a[array2[i]] = true;
+        }
+      }
+      for (let k in a) {
+        diff.push(k);
+      }
+      return diff;
     },
     intersection(array1, array2) {
       if (!Array.isArray(array1) || !Array.isArray(array2)) {
         throw new Error(
           `${JSON.stringify(
             array1
-          )} is not an array. The first function only works on arrays.`
+          )} is not an array. The intersection function only works on arrays.`
+        );
+      }
+      let set1 = new Set(array1);
+      let set2 = new Set(array2);
+      let intersection = new Set([...set1].filter((x) => set2.has(x)));
+      return Array.from(intersection);
+    },
+    zip(array1, array2) {
+      if (!Array.isArray(array1) || !Array.isArray(array2)) {
+        throw new Error(
+          `${JSON.stringify(
+            array1
+          )} is not an array. The zip function only works on arrays.`
+        );
+      }
+      let newArray = array1.map(function (e, i) {
+        return [e, array2[i]];
+      });
+      return newArray;
+    },
+    unzip(array) {
+      if (!Array.isArray(array)) {
+        throw new Error(
+          `${JSON.stringify(
+            array
+          )} is not an array. The unzip function only works on arrays.`
+        );
+      }
+      return "_.unzip isn't working";
+    },
+    union(array1, array2) {
+      if (!Array.isArray(array1) || !Array.isArray(array2)) {
+        throw new Error(
+          `${JSON.stringify(
+            array1
+          )} is not an array. The union function only works on arrays.`
+        );
+      }
+      let concatArray = [...array1, ...array2];
+      let arrayUnion = [...new Set(concatArray)];
+      return arrayUnion;
+    },
+    initial(array, n) {
+      if (!Array.isArray(array)) {
+        throw new Error(
+          `${JSON.stringify(
+            array
+          )} is not an array. The initial function only works on arrays.`
         );
       }
       if (typeof n === "undefined") {
-        var setA = new Set(array1);
-        var setB = new Set(array2);
-        var intersection = new Set([...setA].filter((x) => setB.has(x)));
-        return Array.from(intersection);
+        array.pop();
+        return array;
       }
+      if (!(n >= 1)) {
+        throw new Error(
+          `${JSON.stringify(
+            n
+          )} is not a valid number of items. Please use a number > 0`
+        );
+      }
+      for (let i = 0; i < n; i++) {
+        array.pop();
+      }
+      return array;
+    },
+    rest(array, index) {
+      if (!Array.isArray(array)) {
+        throw new Error(
+          `${JSON.stringify(
+            array
+          )} is not an array. The rest function only works on arrays.`
+        );
+      }
+      if (typeof index === "undefined") {
+        return array;
+      }
+      if (!(index >= 1)) {
+        throw new Error(
+          `${JSON.stringify(
+            index
+          )} is not a valid index. Please use a number > 0`
+        );
+      }
+      let i = index;
+      array.splice(i, 1);
+      return array, "_.rest isn't working";
     },
   };
 })();
